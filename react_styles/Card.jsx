cb8bd19975d4838db753c4d7894a00c468b43937
val3ntinahdz <valentinaleonh8@gmail.com>
2025-10-02 15:18:42 -0600

    Explain inline styling in React

--- react_styles/Card.jsx
+++ react_styles/Card.jsx
@@ -2,8 +2,26 @@
 import styles from "./Card.module.sass";
 
 const Card = () => {
+    // Another way of applying styles in React is the "inline" method (similar to Tailwind)
+    // Nonetheless, the syntax could end up being messy or too large
+    // One best practice is saving the styles in an object and calling it in the style= property
+    const textStyles = {
+        color: "red",
+        fontSize: "2rem",
+        textAlign: "center",
+    }
+
+    // One disadvantage of this way of defining styles is scalability, 
+    // since in larger applications it wouldn't be advisable to have a single variable with a large number of classes in the same component. 
+    // Furthermore, pseudo-classes would no longer be possible.
+
+    // One solution to this problem is the use of modules (separate files) and 
+    // CSS preprocessors that speed up development, since there are usually many components in a React app.
+
+
     return (
-        <div className={ styles.card }>
+
+        <div style={textStyles}>
             <div className={ styles.secondary_card }>
                 <p className={ styles.quote }> 
                     We used to look up at the sky and wonder at our place in the stars. 
